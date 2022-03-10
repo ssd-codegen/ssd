5a20b4a96e2835b84b0bdc803c7a8e2cea111800
hardliner66 <hardliner66@gmail.com>
2022-03-10 14:53:13 +0100

    fix version, fix about

--- src/options.rs
+++ src/options.rs
@@ -3,6 +3,7 @@ use structopt::StructOpt;
 use std::path::PathBuf;
 
 #[derive(Debug, StructOpt)]
+#[structopt(about = "Simple Service Description & Code Generator")]
 pub struct Options {
     #[structopt(subcommand)]
     pub command: Command,
@@ -16,18 +17,24 @@ pub struct BaseData {
 
 #[derive(Debug, StructOpt)]
 pub enum Command {
+    /// Print debug representation of the parsed file
     Debug(BaseData),
+    /// Pretty print the parsed file
     Pretty(BaseData),
+    /// Use a generator with the parsed file
     Generate(GeneratorOptions),
 }
 
 #[derive(Debug, StructOpt)]
 pub struct GeneratorOptions {
+    /// The script to use to generate the file
     pub script: PathBuf,
     #[structopt(flatten)]
     pub base: BaseData,
     #[structopt(long, short)]
+    /// The file which should get written with the output from the generator
     pub out: Option<PathBuf>,
     #[structopt(long, short)]
+    /// Enables debug mode (print and debug function in the script)
     pub debug: bool,
 }
